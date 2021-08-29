4783e1a951981fac905c6c34c9a3bd02f9a4beb5
Аносов Георгий Аркадьевич <cosmovolk@gmail.com>
2021-08-29 11:03:41 +0300

    both added fixed

--- helloworld.cs
+++ helloworld.cs
@@ -4,10 +4,7 @@ class Main
 {
     void Main (string[] args)
     {
-<<<<<<< HEAD
         Console.WriteLine("My new rating");
-=======
         Console.WriteLine("Comment was added");
->>>>>>> comment
     }
 }
